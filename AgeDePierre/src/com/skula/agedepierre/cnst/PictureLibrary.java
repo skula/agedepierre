f06f0d5dca332d265c0c6ad38e1de7cd89bf41d7
Stephane <Stephane@192.168.1.22>
2016-02-13 12:17:34 +0100

    hi

--- AgeDePierre/src/com/skula/agedepierre/cnst/PictureLibrary.java
+++ AgeDePierre/src/com/skula/agedepierre/cnst/PictureLibrary.java
@@ -17,7 +17,6 @@ public class PictureLibrary {
 	public PictureLibrary(Resources res) {
 		this.map = new HashMap<Integer, Bitmap>();
 		this.map.put(R.drawable.background, BitmapFactory.decodeResource(res, R.drawable.background));
-		this.map.put(R.drawable.paper, BitmapFactory.decodeResource(res, R.drawable.paper));
 		this.map.put(R.drawable.pawn_blue, BitmapFactory.decodeResource(res, R.drawable.pawn_blue));
 		this.map.put(R.drawable.pawn_green, BitmapFactory.decodeResource(res, R.drawable.pawn_green));
 		this.map.put(R.drawable.pawn_yellow, BitmapFactory.decodeResource(res, R.drawable.pawn_yellow));
@@ -45,10 +44,20 @@ public class PictureLibrary {
 		this.map.put(R.drawable.building20, BitmapFactory.decodeResource(res, R.drawable.building20));
 		this.map.put(R.drawable.building21, BitmapFactory.decodeResource(res, R.drawable.building21));
 		this.map.put(R.drawable.building22, BitmapFactory.decodeResource(res, R.drawable.building22));
-		this.map.put(R.drawable.item_farm, BitmapFactory.decodeResource(res, R.drawable.item_farm));
-		this.map.put(R.drawable.item_food, BitmapFactory.decodeResource(res, R.drawable.item_food));
+		this.map.put(R.drawable.paper, BitmapFactory.decodeResource(res, R.drawable.paper));
 		this.map.put(R.drawable.item_building, BitmapFactory.decodeResource(res, R.drawable.item_building));
+		this.map.put(R.drawable.item_farm, BitmapFactory.decodeResource(res, R.drawable.item_farm));
+		this.map.put(R.drawable.item_tool, BitmapFactory.decodeResource(res, R.drawable.item_tool));
+		this.map.put(R.drawable.item_food, BitmapFactory.decodeResource(res, R.drawable.item_food));
+		this.map.put(R.drawable.item_wood, BitmapFactory.decodeResource(res, R.drawable.item_wood));
+		this.map.put(R.drawable.item_copper, BitmapFactory.decodeResource(res, R.drawable.item_copper));
+		this.map.put(R.drawable.item_stone, BitmapFactory.decodeResource(res, R.drawable.item_stone));
+		this.map.put(R.drawable.item_stone, BitmapFactory.decodeResource(res, R.drawable.item_stone));
+		this.map.put(R.drawable.item_gold, BitmapFactory.decodeResource(res, R.drawable.item_gold));
 		this.map.put(R.drawable.item_pawn_blue, BitmapFactory.decodeResource(res, R.drawable.item_pawn_blue));
+		this.map.put(R.drawable.item_pawn_green, BitmapFactory.decodeResource(res, R.drawable.item_pawn_green));
+		this.map.put(R.drawable.item_pawn_red, BitmapFactory.decodeResource(res, R.drawable.item_pawn_red));
+		this.map.put(R.drawable.item_pawn_yellow, BitmapFactory.decodeResource(res, R.drawable.item_pawn_yellow));
 	}
 
 	public Bitmap get(int id) {
